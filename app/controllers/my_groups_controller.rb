874af61f7832db6b14295c2ae4b78b4c80bba174
Alex Hoyer <alexhoyer22@gmail.com>
2019-10-25 01:06:10 +0000

    Added bootstrap, devise and navbar

---
+++ app/controllers/my_groups_controller.rb
@@ -0,0 +1,2 @@
+class MyGroupsController < ApplicationController
+end
---
+++ spec/controllers/projects_controller_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe ProjectsController, type: :controller do
+
+  describe "projects#new action" do
+    it "should require the user is an admin user and logged in" do
+
+    end
+
+    it "should successfully show the new form to create a new project" do
+
+    end
+  end
+
+  describe "projects#create action" do
+
+    it "should successfully create a new project in our database" do
+
+    end
+
+    it "should properly deal with validation errors" do
+
+    end
+  end
+
+  describe "projects#edit action" do
+   it "should require the user is an admin user and logged in" do
+
+    end 
+  end
+
+  describe "projects#update action" do
+    it "should allow project infornmation to be updated" do
+
+    end
+  end
+end
